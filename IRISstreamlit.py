fc3752f136e70e8c5ff940bc26d7eb042759fdf7
andrestorres29 <p315463@uach.mx>
2024-09-11 21:37:17 -0600

    Update IRISstreamlit.py

--- IRISstreamlit.py
+++ IRISstreamlit.py
@@ -7,7 +7,6 @@ Created on Wed Sep  4 22:40:07 2024
 import numpy as np
 import pandas as pd
 import streamlit as st 
-import joblib
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
