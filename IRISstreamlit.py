85a52e627cec2949e7300da99e9465bc464a8a0d
andrestorres29 <p315463@uach.mx>
2024-09-11 21:38:39 -0600

    Update IRISstreamlit.py

--- IRISstreamlit.py
+++ IRISstreamlit.py
@@ -7,30 +7,11 @@ Created on Wed Sep  4 22:40:07 2024
 import numpy as np
 import pandas as pd
 import streamlit as st 
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
 from PIL import Image
-
-rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
-class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
-    def __init__(self, add_bedrooms_per_room=True):
-        self.add_bedrooms_per_room = add_bedrooms_per_room
-    def fit(self, X, y=None):
-        return self
-    def transform(self, X):
-        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
-        population_per_household = X[:, population_ix] / X[:, households_ix]
-        if self.add_bedrooms_per_room:
-            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
-            return np.c_[X, rooms_per_household, population_per_household,
-                         bedrooms_per_room]
-        else:
-            return np.c_[X, rooms_per_household, population_per_household] 
-
 
 modeloLR = joblib.load('IRISpipeline.sav')
 
