043624cb444b078e3e507cc047fb6e4dd94ee686
andrestorres29 <p315463@uach.mx>
2024-09-11 21:54:51 -0600

    Update IRISstreamlit.py

--- IRISstreamlit.py
+++ IRISstreamlit.py
@@ -9,7 +9,7 @@ import pandas as pd
 import streamlit as st 
 from PIL import Image
 import joblib
-from sklearn.pipeline import Pipeline, PipelineModel
+from sklearn.pipeline import Pipeline
 
 modeloLR = joblib.load('IRISpipeline.sav')
 
